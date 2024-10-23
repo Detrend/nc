2c08b3759f25938e3ba1bd1ace55fabc4f0d609d
Michal Popek <michal.dobsice@seznam.cz>
2024-10-23 13:33:56 +0200

    something is drawn ... not whole, idk why

--- source/nucledian/engine/editor/editor_module.cpp
+++ source/nucledian/engine/editor/editor_module.cpp
@@ -131,15 +131,16 @@ namespace nc
 
   void Grid::init()
   {
-    points.resize((400 + 1) * 2 * 2);
+    
+    points.resize((8 + 1) * 2 * 2);
 
     size_t i = 0;
-    for (f32 x = -50; x <= 50 + GRID_SIZE / 2; x += GRID_SIZE)
+    for (f32 x = -1; x <= 1 + GRID_SIZE / 2; x += GRID_SIZE)
     {
-      points[i] = vertex_3d(x, -50, 0);
-      points[i + 1] = vertex_3d(x, 50, 0);
-      points[i + 2] = vertex_3d(-50, x, 0);
-      points[i + 3] = vertex_3d(50, x, 0);
+      points[i] = vertex_3d(x, -10, 0);
+      points[i + 1] = vertex_3d(x, 10, 0);
+      points[i + 2] = vertex_3d(-10, x, 0);
+      points[i + 3] = vertex_3d(10, x, 0);
 
       i += 4;
     }
@@ -177,7 +178,7 @@ namespace nc
     // bind to VBO
     glGenBuffers(1, &vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * points.size(), &points.front(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 3 * points.size(), &points[0], GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -186,30 +187,33 @@ namespace nc
     glGenVertexArrays(1, &vertexArrayBuffer);
     glBindVertexArray(vertexArrayBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(points) * 3, &points.front(), GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points) * 3 * sizeof(float), &points[0], GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
   }
 
   void Grid::render_grid()
   {
+    glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
     
     glUseProgram(shaderProgram);  
+    
+    glEnableVertexAttribArray(0);
+
     glLineWidth(5);
     glBindVertexArray(vertexArrayBuffer);
     glDrawArrays(GL_LINES, 0, sizeof(points));
-
-    
+    glDisableVertexAttribArray(0);
   }
 
   Grid::~Grid()
   {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    glDeleteVertexArrays(1, &vertexArrayBuffer);
   }
 
   
