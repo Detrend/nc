664041ee2b9db96a788bcaf55bb1e3ac5b1b9e40
Michal Popek <michal.dobsice@seznam.cz>
2025-03-06 18:03:46 +0100

    start of InputSystem::handle_app_event

--- source/nucledian/engine/input/input_system.cpp
+++ source/nucledian/engine/input/input_system.cpp
@@ -52,7 +52,56 @@ void InputSystem::override_player_inputs(const PlayerSpecificInputs& new_inputs)
 //==============================================================================
 void InputSystem::handle_app_event([[maybe_unused]]const SDL_Event& event)
 {
-  // TODO
+  // expected to do this after COPYing previous input to current
+
+  switch (event.type) 
+  {
+  case SDL_MOUSEMOTION:
+    f32 xrel = event.motion.xrel * 1.0f; // needs to be replaced by sensitivity
+    f32 yrel = event.motion.yrel * 1.0f;
+    m_current_inputs.player_inputs.analog[PlayerAnalogInputs::look_horizontal] = xrel;
+    m_current_inputs.player_inputs.analog[PlayerAnalogInputs::look_vertical] = yrel;
+  case SDL_KEYDOWN:
+    switch (event.key.keysym.scancode) 
+    {
+    case SDL_SCANCODE_W:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys & (1 << PlayerKeyInputs::forward);
+      break;
+    case SDL_SCANCODE_S:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys & (1 << PlayerKeyInputs::backward);
+      break;
+    case SDL_SCANCODE_A:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys & (1 << PlayerKeyInputs::left);
+      break;
+    case SDL_SCANCODE_D:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys & (1 << PlayerKeyInputs::right);
+      break;
+    default:
+      break;
+    }
+    break;
+  case SDL_KEYUP:
+    switch (event.key.keysym.scancode)
+    {
+    case SDL_SCANCODE_W:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys ^ (1 << PlayerKeyInputs::forward);
+      break;
+    case SDL_SCANCODE_S:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys ^ (1 << PlayerKeyInputs::backward);
+      break;
+    case SDL_SCANCODE_A:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys ^ (1 << PlayerKeyInputs::left);
+      break;
+    case SDL_SCANCODE_D:
+      m_current_inputs.player_inputs.keys = m_current_inputs.player_inputs.keys ^ (1 << PlayerKeyInputs::right);
+      break;
+    default:
+      break;
+    }
+    break;
+  default:
+    break;
+  }
 }
 
 //==============================================================================
