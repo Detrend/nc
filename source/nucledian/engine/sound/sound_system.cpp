c172bd51c6bc537b9dd013a052555ce63f08aa33
MarkusSecundus <kubahronik.g@gmail.com>
2025-05-25 00:01:43 +0200

    sound system WIP

--- source/nucledian/engine/sound/sound_system.cpp
+++ source/nucledian/engine/sound/sound_system.cpp
@@ -95,19 +95,36 @@ namespace nc
         }
     }
 
-    void SoundSystem::play_oneshot(const SoundResource& sound)
+    Mix_Chunk* SoundSystem::get_loaded_chunk(const SoundResource& resource)
     {
-        (void)sound;
+        auto it = loaded_chunks.find(resource);
+        if (it == loaded_chunks.end()) {
+            Mix_Chunk*const ret = Mix_LoadWAV(resource);
+            nc_assert(ret);
+            loaded_chunks[resource] = ret;
+            return ret;
+        }
+        return it->second;
     }
 
-    void SoundSystem::play(const SoundResource& sound)
+    void SoundSystem::play_oneshot(const SoundResource& sound)
     {
-        (void)sound;
+        play(sound);
+    }
+
+    SoundHandle SoundSystem::play(const SoundResource& sound, const bool should_loop /* = false*/)
+    {
+        Mix_Chunk *const chunk = get_loaded_chunk(sound);
+        nc_assert(chunk);
+        const int channel = Mix_PlayChannel(-1, chunk, should_loop ? -1 : 0);
+        nc_assert(channel != -1);
+        return SoundHandle(chunk, channel, generate_next_version_id());
     }
 
     //==============================================================================
     void SoundSystem::terminate()
     {
+        
     }
 
     //==============================================================================
@@ -117,31 +134,82 @@ namespace nc
     }
 
 
-    void SoundHandle::play()
+    void SoundHandle::set_paused(const bool should_be_paused)
     {
-    }
-
-    void SoundHandle::pause()
-    {
+        nc_assert(check_is_valid());
+        if(should_be_paused)
+            Mix_Pause(this->channel);
+        Mix_Resume(this->channel);
     }
 
     void SoundHandle::kill()
     {
+        nc_assert(check_is_valid());
+        Mix_HaltChannel(this->channel);
+        
+    }
+
+    static int volume_from01(float volume01) {
+        return static_cast<int>(clamp(volume01, 0.0f, 1.0f) * MIX_MAX_VOLUME); 
+    }
+    static float volume_to01(int mixer_volume) {
+        return clamp(static_cast<float>(mixer_volume) / MIX_MAX_VOLUME, 0.0f, 1.0f);
     }
 
     void SoundHandle::set_volume(float volume01)
     {
-        (void)volume01;
+        nc_assert(check_is_valid());
+        const int ret = Mix_VolumeChunk(this->chunk, volume_from01(volume01));
+        (void)ret;
+        nc_assert(ret >= 0);
     }
 
-    void SoundHandle::set_playpoint(float point01)
+
+    bool SoundHandle::is_paused() const
     {
-        (void)point01;
+        nc_assert(check_is_valid());
+        const int ret = Mix_Paused(this->channel);
+        if (ret == 1) return true;
+        if (ret == 0) return false;
+        nc_assert(false, "invalid paused value '{}'", ret);
+        return false;
     }
 
     bool SoundHandle::is_playing() const
     {
+        nc_assert(check_is_valid());
         return false;
+    }
+
+    float SoundHandle::get_volume()
+    {
+        nc_assert(check_is_valid());
+        const int ret = Mix_VolumeChunk(this->chunk, -1);
+        nc_assert(ret >= 0);
+        return volume_to01(ret);
+    }
+
+    TweenBase& SoundHandle::do_volume(float end_volume01, float duration_seconds, easingsf::easing_func_t ease) 
+    {
+        SoundSystem& sys = get_engine().get_module<SoundSystem>();
+
+        return get_engine().get_module<TweenSystem>().tween(get_volume(), end_volume01, duration_seconds, ease, [this, &sys](float value) {
+            if (!this->is_playing())
+                return TweenSetterReturnValue::tween_is_invalid;;
+            if (sys.active_chunks.find(this->channel)->second.version != this->version)
+                return TweenSetterReturnValue::tween_is_invalid;
+            this->set_volume(value);
+            return TweenSetterReturnValue::ok;
+            });
+    }
+
+    bool SoundHandle::check_is_valid() const {
+        if (!this->chunk) return false;
+        if (this->channel < 0) return false;
+
+        auto& sound_system = get_engine().get_module<SoundSystem>();
+        auto it = sound_system.active_chunks.find(channel);
+        return it != sound_system.active_chunks.end() && it->second.version == this->version;
     }
 
 }
