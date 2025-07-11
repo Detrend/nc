5b63fcf57452db424d31aad557270a37f07a2aa0
MarkusSecundus <kubahronik.g@gmail.com>
2025-07-12 00:36:36 +0200

    cleanup - remove `clockwise_test`

--- scripts/clockwise_test.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _process(delta: float) -> void:
-	var is_clockwise : int = GeometryUtils.is_clockwise_triangle($A.global_position, $B.global_position, $C.global_position)
-	print("clockwise: {0}".format([is_clockwise]))
