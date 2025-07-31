433f9e91f2c3a49f3d7386df687838571e2c7fd4
Michal Popek <michal.dobsice@seznam.cz>
2025-07-31 09:55:00 +0200

    unbind at the end of init

--- source/nucledian/engine/ui/user_interface_module.cpp
+++ source/nucledian/engine/ui/user_interface_module.cpp
@@ -31,6 +31,8 @@ namespace nc
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
     glEnableVertexAttribArray(0);
     
+    glDisableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 
     return true;
