eb686452abe5f2579645cb5bfebafe70c031a0c5
Michal Popek <michal.dobsice@seznam.cz>
2025-08-11 15:02:25 +0200

    in button check

--- source/nucledian/engine/ui/ui_button.cpp
+++ source/nucledian/engine/ui/ui_button.cpp
@@ -2,5 +2,30 @@
 
 namespace nc
 {
+  UiButton::UiButton(GuiTexture* texture, std::function<void> func)
+  {
+    this->texture = texture;
+    this->func = func;
+  }
 
+  bool UiButton::is_point_in_rec(vec2 point)
+  {
+    vec2 position = texture->get_position();
+    vec2 scale = texture->get_scale();
+
+    vec2 start = position - scale / 2.0f;
+    vec2 end = position + scale / 2.0f;
+
+    if (point.x < start.x || point.x > end.x)
+    {
+      return false;
+    }
+
+    if (point.y < start.y || point.y > end.y)
+    {
+      return false;
+    }
+
+    return true;
+  }
 }
--- source/nucledian/engine/ui/ui_button.h
+++ source/nucledian/engine/ui/ui_button.h
@@ -1,14 +1,21 @@
+#include <engine/ui/ui_texture.h>
+
 #include <vector>
+#include <functional>
 
 namespace nc
 {
   class UiButton
   {
   public:
+    UiButton(GuiTexture* texture, std::function<void> func);
+    bool is_point_in_rec(vec2 point);
+
 
   private:
-
-
+    GuiTexture* texture;
+    bool isHover = false;
+    std::function<void> func;
   };
 
   class UiButtonManager
