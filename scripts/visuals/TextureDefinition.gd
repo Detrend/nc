e7a574ca4c37f02f072d95e64e464a5a0ec3c660
MarkusSecundus <kubahronik.g@gmail.com>
2025-08-19 01:13:15 +0200

    add support for tile rotation randomization

--- scripts/visuals/TextureDefinition.gd
+++ scripts/visuals/TextureDefinition.gd
@@ -6,6 +6,8 @@ extends ITextureDefinition
 @export var _id : String
 @export var scale : float = 1.0
 @export_range(0.0, 360.0) var rotation : float = 0.0
+@export_range(1, 32) var tile_rotations_count : int = 1
+@export_range(0.0, 360.0) var tile_rotation_increment : float = 90.0
 @export var should_show : bool = true
 
 var id: String:
@@ -27,6 +29,8 @@ func append_info(out: Array[Dictionary], begin_height: float, end_height: float,
 		info["rotation"] = deg_to_rad(base_rotation_deg + custom_rotation_deg)
 		var offset := ctx.target_sector.data.wall_texturing_offset if (ctx.subject_type == ITextureDefinition.TexturingSubjectType.Wall) else ctx.target_sector.data.texturing_offset
 		info["offset"] = [offset.x, offset.y]
+		info["tile_rotations_count"] = tile_rotations_count if tile_rotations_count else 0
+		info["tile_rotation_increment"] = deg_to_rad(tile_rotation_increment)
 		
 	if ctx.subject_type == TexturingSubjectType.Wall:
 		info["begin_height"] = begin_height
