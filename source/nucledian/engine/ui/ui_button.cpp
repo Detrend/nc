31b96a1739c7ded645e6b578c9ef8192cfecbbaa
Michal Popek <michal.dobsice@seznam.cz>
2025-09-03 10:09:24 +0200

    filling up the skeleton

--- source/nucledian/engine/ui/ui_button.cpp
+++ source/nucledian/engine/ui/ui_button.cpp
@@ -1,4 +1,5 @@
 #include <engine/ui/ui_button.h>
+#include <SDL.h>
 
 namespace nc
 {
@@ -27,5 +28,72 @@ namespace nc
     }
 
     return true;
+  }
+
+  //============================================================================================
+
+  MenuManager::MenuManager()
+  {
+    main_menu_page = new MainMenuPage();
+    options_page = new OptionsPage();
+    load_game_page = new LoadGamePage();
+    save_game_page = new SaveGamePage();
+  }
+
+  //============================================================================================
+
+  MenuManager::~MenuManager()
+  {
+    delete main_menu_page;
+    delete options_page;
+    delete load_game_page;
+    delete save_game_page;
+  }
+
+  //=============================================================================================
+
+  vec2 MenuManager::get_normalized_mouse_pos()
+  {
+    int mouse_x, mouse_y;
+
+    SDL_GetMouseState(&mouse_x, &mouse_y);
+
+    u32 width = GraphicsSystem::WINDOW_WIDTH;
+    u32 height = GraphicsSystem::WINDOW_HEIGHT;
+
+    f32 half_width = width / 2.0f;
+    f32 half_height = width / 2.0f;
+
+    //normalize between <0, 2>, then minus one to move it to <-1, 1>
+    f32 norm_x = mouse_x / half_width - 1.0f;
+    f32 norm_y = mouse_y / half_height - 1.0f;
+
+    return vec2(norm_x, - norm_y);
+  }
+
+  //=============================================================================================
+
+  void MenuManager::set_visible(bool visibility)
+  {
+    visible = visibility;
+    current_page = MAIN;
+  }
+
+  //============================================================================================
+
+  void MenuManager::draw()
+  {
+  }
+
+  //===========================================================================================
+
+  MainMenuPage::MainMenuPage()
+  {
+  }
+
+  //============================================================================================
+
+  MainMenuPage::~MainMenuPage()
+  {
   }
 }
--- source/nucledian/engine/ui/ui_button.h
+++ source/nucledian/engine/ui/ui_button.h
@@ -1,11 +1,21 @@
 #include <engine/ui/ui_texture.h>
 #include <glad/glad.h>
+#include <engine/graphics/graphics_system.h>
 
 #include <vector>
 #include <functional>
 
 namespace nc
 {
+  enum MenuPages
+  {
+    MAIN,
+    NEW_GAME,
+    OPTIONS,
+    LOAD,
+    SAVE
+  };
+
   class UiButton
   {
   public:
@@ -21,7 +31,21 @@ namespace nc
 
   class MainMenuPage
   {
+  public:
+    MainMenuPage();
+    ~MainMenuPage();
 
+  private:
+    UiButton* new_game_button = nullptr;
+    UiButton* options_button = nullptr;
+    UiButton* load_button = nullptr;
+    UiButton* save_button = nullptr;
+    UiButton* quit_button = nullptr;
+  };
+
+  class NewGamePage
+  {
+    
   };
 
   class OptionsPage
@@ -39,10 +63,25 @@ namespace nc
 
   };
 
-  
-
   class MenuManager
   {
+  public:
+    MenuManager();
+    ~MenuManager();
+    
+    vec2 get_normalized_mouse_pos();
 
+    void set_visible(bool visibility);
+    void draw();
+
+  private:
+    MainMenuPage* main_menu_page = nullptr;
+    OptionsPage* options_page = nullptr;
+    LoadGamePage* load_game_page = nullptr;
+    SaveGamePage* save_game_page = nullptr;
+
+    MenuPages current_page = MenuPages::MAIN;
+
+    bool visible = false;
   };
 }
