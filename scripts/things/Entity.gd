6c8542f8fb922d344a5acaf981ff074e83eb5db3
MarkusSecundus <kubahronik.g@gmail.com>
2025-11-08 00:02:48 +0100

    add support for multiple enemy types (Cultist and Possessed)

--- scripts/things/Entity.gd
+++ scripts/things/Entity.gd
@@ -2,6 +2,8 @@
 extends Thing
 class_name Entity
 
+@export_enum("Cultist:0", "Possessed:1") var entity_type : int = 0
+
 func get_export_category()->String: return "entities"
 
 
@@ -9,6 +11,7 @@ func custom_export(_s: Sector, output: Dictionary)->void:
 	super.custom_export(_s, output)
 	output["is_player"] = false
 	output["forward"] = TextUtils.vec3_to_array(_get_forward_direction())
+	output["entity_type"] = entity_type
 	
 func _get_forward_direction()->Vector3:
 	var rot := Vector2(0.0, -1.0).rotated(self.global_rotation)
