4c3d481a0afaf31772b3aa053734e0cfb518ff98
Detrend <0.optima.0@gmail.com>
2025-11-09 21:20:05 +0100

    Fixed compilation, replaced level name and chapter name with std::string because the std::array version sucked

--- source/nucledian/engine/player/level_types.h
+++ source/nucledian/engine/player/level_types.h
@@ -1,11 +1,13 @@
 // Project Nucledian Source File
 #pragma once
 
+#include <common.h>
 #include <types.h>
 #include <metaprogramming.h>
 
 #include <array>
 #include <format>
+#include <string>
 
 namespace nc
 {
@@ -13,28 +15,27 @@ namespace nc
 constexpr u64 CHAPTER_ID_LENGTH = 24;
 constexpr u64 LEVEL_NAME_LENGTH = 24;
 
-using ChapterID = std::array<char, CHAPTER_ID_LENGTH>;
-using LevelName = std::array<char, LEVEL_NAME_LENGTH>;
+// TODO: Replace with static string.
+using ChapterID = std::string;
+using LevelName = std::string;
 
+inline const LevelName   INVALID_LEVEL_NAME    = std::string{"<invalid_level>"};
+inline const ChapterID   INVALID_CHAPTER_ID    = std::string{"<invalid_chapter>"};
+inline const std::string LEVELS_DIRECTORY_PATH = std::string{".\\content\\levels"};
 
-#define make_arraystring(length, str) std::to_array((const char[(length)]) (str))
-
-
-constexpr LevelName INVALID_LEVEL_NAME = make_arraystring(LEVEL_NAME_LENGTH, "<invalid_level>");
-constexpr ChapterID INVALID_CHAPTER_ID = make_arraystring(CHAPTER_ID_LENGTH, "<invalid_chapter>");
-
-const std::string LEVELS_DIRECTORY_PATH = ".\\content\\levels";
-
-static inline std::string get_full_level_path(const LevelName& level_name) { return std::format("{0}\\{1}.json", LEVELS_DIRECTORY_PATH, level_name.data()); }
+inline std::string get_full_level_path(const LevelName& level_name)
+{
+  return std::format("{0}\\{1}.json", LEVELS_DIRECTORY_PATH, level_name.data());
+}
 
 namespace Levels
 {
-  static constexpr LevelName LEVEL_1 = make_arraystring(LEVEL_NAME_LENGTH, "level_test1");
-  static constexpr LevelName LEVEL_2 = make_arraystring(LEVEL_NAME_LENGTH, "level_test2");
+inline const LevelName LEVEL_1 = "level_test1";
+inline const LevelName LEVEL_2 = "level_test2";
 }
 
-
-static auto LevelsDB = std::to_array({
+static auto LevelsDB = std::to_array
+({
   Levels::LEVEL_1,
   Levels::LEVEL_2
 });
