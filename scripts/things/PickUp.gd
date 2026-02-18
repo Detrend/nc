b138f992b23db2712385ecc73348a1380298e6b6
MarkusSecundus <kubahronik.g@gmail.com>
2026-02-18 16:01:32 +0100

    add prefabs for shotgun and plasma pickups

--- scripts/things/PickUp.gd
+++ scripts/things/PickUp.gd
@@ -2,11 +2,22 @@
 extends Thing
 class_name PickUp
 
+enum PickUpType{
+	HpSmall,
+	HpBig,
+	Shotgun,
+	PlasmaRifle,
+	NailGun,
+	ShotgunAmmo,
+	PlasmaAmmo,
+	NailAmmo
+}
+
 @export_group("Internal")
-@export var type_id : int
+@export var type_id : PickUpType = PickUpType.HpSmall
 
 func get_export_category()->String: return "pickups"
 
 func custom_export(_s: Sector, output: Dictionary)->void:
 	super.custom_export(_s, output)
-	output["type"] = self.type_id
+	output["type"] = int(self.type_id)
