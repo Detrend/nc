63f22818901eca398f26956d1667c600f73aa2f7
matus-rozek <matus.rozek@warhorsestudios.cz>
2026-03-15 15:04:10 +0100

    Hotfix for entities disappearing around the portals

--- source/nucledian/engine/graphics/renderer.cpp
+++ source/nucledian/engine/graphics/renderer.cpp
@@ -763,7 +763,9 @@ void Renderer::render_entities(const CameraData& camera) const
         //const ResLifetime lifetime = texture.get_lifetime();
         //groups[lifetime].insert(entity);
 
-        if (m_entity_checker.check_redundant(id.as_u32(), camera_pos).first)
+				// TODO: This causes enemies to disappear in certain situations.
+        //if (m_entity_checker.check_redundant(id.as_u32(), camera_pos).first)
+        if (true)
         {
           auto& group = groups[cast<u64>(ResLifetime::Game)];
           EntityRenderData& render_data = group[id.as_u32()];
