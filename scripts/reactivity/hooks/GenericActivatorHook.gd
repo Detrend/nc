2b2ae748fd0cce867e09d38559572c2aa19e4835
MarkusSecundus <kubahronik.g@gmail.com>
2026-03-25 21:51:53 +0100

    add minor error check

--- scripts/reactivity/hooks/GenericActivatorHook.gd
+++ scripts/reactivity/hooks/GenericActivatorHook.gd
@@ -10,4 +10,7 @@ func get_type()->String: return type
 func do_export(out: Dictionary)->void:
 	super.do_export(out)
 	for k in data:
+		if out.has(k): 
+			ErrorUtils.report_error("Duplicit key '{0}' in {1}".format([k, NodeUtils.get_full_name(self)]))
+			continue
 		out[k] = TextUtils.sanitize_json_object(data[k])
