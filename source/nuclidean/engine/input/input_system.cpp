95f320a10bf315a8887147369be97063e14b9520
PopikM <michal.dobsice@seznam.cz>
2026-05-15 12:00:28 +0200

    mouse wheel input

--- source/nuclidean/engine/input/input_system.cpp
+++ source/nuclidean/engine/input/input_system.cpp
@@ -10,6 +10,8 @@
 
 #include <SDL2/include/SDL.h>
 
+#include <game/weapons.h>
+
 #include <utility> // std::pair
 
 namespace nc
@@ -55,7 +57,6 @@ void handle_player_input(GameInputs& inputs)
   // Look around
   int x, y;
   u32 mouse_code = SDL_GetRelativeMouseState(&x, &y);
-
   f32 sensitivity = get_engine().get_module<InputSystem>().get_sensitivity();
   inputs.player_inputs.analog[Analogues::look_vertical  ] = y * sensitivity;
   inputs.player_inputs.analog[Analogues::look_horizontal] = x * sensitivity;
@@ -118,9 +119,57 @@ void InputSystem::update_window_and_pump_messages()
 
     // consume the message ourselves
     this->handle_app_event(event);
+
+    if (event.type == SDL_MOUSEWHEEL)
+    {
+      handle_mouse_wheel(event);
+    }
   }
 
   handle_player_input(m_current_inputs);
+}
+
+//====================================================================
+void InputSystem::handle_mouse_wheel(SDL_Event& event)
+{
+  if (event.wheel.y > 0)
+  {
+    WeaponType weapon = GameHelpers::get().get_player()->get_equipped_weapon();
+    WeaponType next_weapon = weapon;
+    do
+    {
+      next_weapon = (next_weapon + 1);
+      if (next_weapon >= WeaponTypes::count)
+      {
+        next_weapon = 0;
+      }
+
+      if (GameHelpers::get().get_player()->has_weapon(next_weapon))
+      {
+        m_current_inputs.player_inputs.keys |= 1 << (PlayerKeyInputs::weapon_0 + next_weapon);
+        break;
+      }
+    } while (next_weapon != weapon);
+  }
+  else if (event.wheel.y < 0)
+  {
+    WeaponType weapon = GameHelpers::get().get_player()->get_equipped_weapon();
+    WeaponType next_weapon = weapon;
+    do
+    {
+      next_weapon = (next_weapon - 1);
+      if (next_weapon == INVALID_WEAPON_TYPE)
+      {
+        next_weapon = WeaponTypes::count - 1;
+      }
+
+      if (GameHelpers::get().get_player()->has_weapon(next_weapon))
+      {
+        m_current_inputs.player_inputs.keys |= 1 << (PlayerKeyInputs::weapon_0 + next_weapon);
+        break;
+      }
+    } while (next_weapon != weapon);  
+  }
 }
 
 //==============================================================================
