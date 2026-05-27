4c7455cbd90db11fb7c272523434d154e6d8c0be
Petr Kotáb <petrkotab99@gmail.com>
2026-05-28 01:19:33 +0200

    removed accidental normal mimmaps

--- source/nuclidean/engine/graphics/resources/texture.cpp
+++ source/nuclidean/engine/graphics/resources/texture.cpp
@@ -519,7 +519,7 @@ void TextureManager::finish_load(ResLifetime lifetime)
     glGenTextures(1, &bundle.normal_handle);
     glBindTexture(GL_TEXTURE_2D, bundle.normal_handle);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, target_width, target_height, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
@@ -554,8 +554,6 @@ void TextureManager::finish_load(ResLifetime lifetime)
       );
       stbi_image_free(load_data.normal_data);
     }
-
-    glGenerateMipmap(GL_TEXTURE_2D);
   }
 
   // specular texture atlas
